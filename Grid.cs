032fbaa2c06429c61c7df46848bc24ede4141087
RichnNL <dyer.richard.j@hotmail.nl>
2016-06-17 19:31:07 +0200

    Add files via upload

---
+++ Grid.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pipes
+{
+    class Grid
+    {
+        Panel panel;
+        PointP xy;
+        float height;
+        float width;
+        float xSpace;
+        float ySpace;
+        Graphics graph;
+        public int lines { get; set; }
+        Pen pen = new Pen(Brushes.Transparent);
+
+        public Grid(Panel pan, int NumberOfLines)
+        {
+            this.panel = pan;
+            this.xy.X = 0f;
+            this.xy.Y = 0f;
+            this.height = pan.Height;
+            this.width = pan.Width;
+            graph = pan.CreateGraphics();
+            this.lines = NumberOfLines;
+
+        }
+        void drawGrid(int NumberOfLines)
+        {
+            lines = NumberOfLines;
+            xy.X = 0f;
+            xy.Y = 0f;
+            xSpace = width / NumberOfLines;
+            ySpace = height / NumberOfLines;
+
+            //Draws the Vertical Grid Lines
+            for (int i = 0; i <= lines; i++)
+            {
+                graph.DrawLine(pen, xy.X, xy.Y, xy.X, height);
+
+            }
+            xy.X = 0f;
+            //Draw the Horizontal Grid Lines
+            for (int i = 0; i < lines; i++)
+            {
+                graph.DrawLine(pen, xy.X, xy.Y, width, xy.Y);
+
+            }
+
+        }
+        PointP returnMousePosition(Point mousePosition)
+        {
+            PointP gridSpace = null;
+            gridSpace.X = Convert.ToSingle(mousePosition.X / xSpace);
+            gridSpace.Y = Convert.ToSingle(mousePosition.Y / ySpace);
+            Math.Floor(gridSpace.X);
+            Math.Floor(gridSpace.Y);
+            return gridSpace;
+        }
+        public bool enlarge()
+        {
+            int max = 15;
+            if (lines >= max)
+            {
+                lines = 15;
+                drawGrid(lines);
+                return false;
+            }
+            else
+            {
+                lines++;
+                drawGrid(lines);
+                return true;
+            }
+        }
+        public bool lessen()
+        {
+            int min = 6;
+            if (lines <= min)
+            {
+                lines = 6;
+                drawGrid(lines);
+                return false;
+            }
+            else
+            {
+                lines--;
+                drawGrid(lines);
+                return true;
+            }
+        }
+
+    }
+}
---
+++ SaveLoad.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pipes
+{
+    class SaveLoad
+    {
+
+        public string filename { get; set; }
+        public SaveLoad()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "bin|*.bin";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filename = ofd.FileName;
+            }
+            else
+            {
+                bool b = updateFileName();
+                while (!b)
+                {
+                    b = updateFileName();
+                };
+
+            }
+
+        }
+        public bool save(List<Component> CompList)
+        {
+            if (filename != null)
+            {
+
+                BinaryFormatter bf = new BinaryFormatter();
+                using (FileStream s = new FileStream(filename, FileMode.OpenOrCreate))
+                {
+                  
+                        bf.Serialize(s, CompList);
+                    
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        public List<Component> load()
+        {
+            List<Component> tempList = null;
+            BinaryFormatter bf = new BinaryFormatter();
+            using (FileStream fs = new FileStream(filename, FileMode.Open))
+            {
+                tempList = bf.Deserialize(fs);
+               
+                return tempList;
+
+            }
+
+        }
+        public bool updateFileName()
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "bin|*.bin";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filename = ofd.FileName;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
